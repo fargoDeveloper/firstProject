51bfc19b29ecc19c88716cc5544217b7e6d312e0
MNedoshovenko <f.test.05.2021@gmail.com>
2021-07-19 13:23:43 +0300

    Create unit test for "resource_loader.py"

--- serviceApp/resource_loader.py
+++ serviceApp/resource_loader.py
@@ -6,10 +6,15 @@ class ResourceLoader:
     @staticmethod
     def load_from_file(name_file):
         list_from_resource = []
-        file = open(name_file, "rt")
+        try:
+            file = open(name_file, "rt")
 
-        for line in file:
-            if len(line) != 0:
-                list_from_resource.append(line)
-        file.close()
-        return list_from_resource
+            for line in file:
+                if len(line) != 0:
+                    list_from_resource.append(line)
+            file.close()
+            return list_from_resource
+        except OSError:
+            print("     ----------     ")
+            print("--- FileNotFound ---")
+            print("     ----------     ")
--- serviceApp/testCases/test_resource_loader.py
+++ serviceApp/testCases/test_resource_loader.py
@@ -12,6 +12,14 @@ class TestResourceLoader(unittest.TestCase):
         name_file = "test_file.txt"
         self.assertEqual(7, len(self.test_rl.load_from_file(name_file)))
 
+    def test_load_from_empty_file(self):
+        name_file = "test_empty_file.txt"
+        self.assertEqual(0, len(self.test_rl.load_from_file(name_file)))
+
+    def test_load_throws_exception(self):
+        name_file = "test_some_file.txt"
+        self.assertRaises(TypeError, self.test_rl.load_from_file(name_file))
+
 
 if __name__ == "__main__":
     unittest.main()
