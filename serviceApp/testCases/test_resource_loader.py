928a4cb3c0faff5e3274d49769c3c01c265d14c6
MNedoshovenko <f.test.05.2021@gmail.com>
2021-07-20 21:52:20 +0300

    Refactoring the code in the "test_resource_loader.py" module

--- serviceApp/testCases/test_resource_loader.py
+++ serviceApp/testCases/test_resource_loader.py
@@ -6,19 +6,21 @@ from serviceApp.resource_loader import ResourceLoader
 class TestResourceLoader(unittest.TestCase):
 
     def setUp(self):
-        self.test_rl = ResourceLoader()
+        name_file1 = "test_file.txt"
+        name_file2 = "test_empty_file.txt"
+        name_file3 = "test_some_file.txt"
+        self.test_rl_file = ResourceLoader(name_file1)
+        self.test_rl_empty_file = ResourceLoader(name_file2)
+        self.test_rl_some_file = ResourceLoader(name_file3)
 
     def test_load_from_file(self):
-        name_file = "test_file.txt"
-        self.assertEqual(7, len(self.test_rl.load_from_file(name_file)))
+        self.assertEqual(7, len(self.test_rl_file.load_from_file()))
 
     def test_load_from_empty_file(self):
-        name_file = "test_empty_file.txt"
-        self.assertEqual(0, len(self.test_rl.load_from_file(name_file)))
+        self.assertEqual(0, len(self.test_rl_empty_file.load_from_file()))
 
-    def test_load_throws_exception(self):
-        name_file = "test_some_file.txt"
-        self.assertRaises(TypeError, self.test_rl.load_from_file(name_file))
+    def test_load_some_file(self):
+        self.assertRaises(TypeError, self.test_rl_some_file.load_from_file())
 
 
 if __name__ == "__main__":
